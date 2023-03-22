66456a2009e1915aea6d88006f0ac7d8e2023b32
pwachnicka <pwachnicka@sii.pl>
2023-03-22 11:24:29 +0100

    Creating tests - in progress

--- tests/AuthorTest.php
+++ tests/AuthorTest.php
@@ -1,10 +1,10 @@
 <?php
 
+use App\Models\Author;
 use Tests\TestCase;
 
 class AuthorTest extends TestCase
 {
-
     /**
      * /api/authors [GET]
      */
@@ -52,16 +52,7 @@ class AuthorTest extends TestCase
      */
     public function testShouldCreateAuthor()
     {
-        $parameters = [
-            'name' => 'TesingTesters',
-            'email' => 'test@test.com',
-            'github' => 'tttt',
-            'twitter' => 'tttt',
-            'location' => 'Spain',
-            'latest_article_published' => 'Testing Lumen API With PHPUnit Tests'
-        ];
-
-        $this->post("/api/authors", $parameters, []);
+        $this->post("/api/authors", Author::factory()->definition(), []);
         $this->seeStatusCode(201);
         $this->seeJsonStructure([
             'id',
@@ -74,5 +65,39 @@ class AuthorTest extends TestCase
             'created_at',
             'updated_at',
         ]);
+    }
+
+    /**
+     * /api/authors/id [PUT]
+     */
+    public function testShouldUpdateAuthor()
+    {
+        $parameters = [
+            'latest_article_published' => 'Testing PUT method with feature test'
+        ];
+
+        $this->put('/api/authors/4', $parameters, []);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'id',
+            'name',
+            'email',
+            'github',
+            'twitter',
+            'location',
+            'latest_article_published',
+            'created_at',
+            'updated_at',
+        ]);
+    }
+
+    /**
+     * /api/authors/id [DELETE]
+     */
+    public function testShouldDeleteAuthor()
+    {
+        $this->delete('api/authors/11', [], []);
+        $this->seeStatusCode(200);
+        $this->seeJsonEquals(['message' => 'Delete succesfully']);
     }
 }
